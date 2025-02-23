fbb43192f28d30210d591fe150b6b481dcb7d866
Montasir <161086149+KMontasir@users.noreply.github.com>
2025-02-23 09:22:19 +0100

    Update 03_pdc_config_ad.ps1

--- functions/pdc/03_pdc_config_ad.ps1
+++ functions/pdc/03_pdc_config_ad.ps1
@@ -7,17 +7,14 @@ Add-DnsServerPrimaryZone -NetworkId $Network -DynamicUpdate Secure -ReplicationS
 Add-DnsServerResourceRecordPtr -Name $PrimaryDNS_LastOctet -ZoneName $ReverseZone -PtrDomainName "${PDC_Hostname}.${DomainName}"
 
 # Enregistre le second serveur DNS
-# Création d'un enregistrement A pour le serveur secondaire DNS
-Add-DnsServerResourceRecordA -Name $SDC_Hostname -ZoneName $DomainName -IPv4Address $SecondaryDNS -TimeToLive $Record_TimeToLive -PassThru
+# Création d'un enregistrement A et PTR pour le serveur secondaire DNS
+Add-DnsServerResourceRecordA -Name $SDC_Hostname -ZoneName $DomainName -IPv4Address $SecondaryDNS -TimeToLive $Record_TimeToLive -CreatePtr -PassThru
 
 # Ajouter un enregistrement NS pour le serveur secondaire dans la zone DNS principale
 Add-DnsServerResourceRecord -Name "@" -NS -ZoneName $DomainName -NameServer "$SDC_Hostname.$DomainName" -PassThru
 
 # Ajouter un enregistrement NS pour le serveur secondaire dans la zone de recherche inversée
 Add-DnsServerResourceRecord -Name "@" -NS -ZoneName $ReverseZone -NameServer "$SDC_Hostname.$DomainName" -PassThru
-
-# Ajouter manuellement l'enregistrement PTR pour le serveur secondaire dans la zone inverse
-Add-DnsServerResourceRecord -Name $SecondaryDNS_LastOctet -PTR -ZoneName $ReverseZone -PtrDomainName "$SDC_Hostname.$DomainName" -PassThru
 
 # Installation du rôle DHCP sur PDC
 Install-WindowsFeature -Name 'DHCP' -IncludeManagementTools
@@ -48,7 +45,3 @@ Get-Service -Name DHCPServer
 
 # Vérification des étendues DHCP configurées
 Get-DhcpServerv4Scope
-
-# Execution des scripts supplementaires
-#.\functions\pdc\config_adds.ps1
-#.\functions\pdc\config_gpo.ps1
